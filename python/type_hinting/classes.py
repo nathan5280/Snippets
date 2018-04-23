7316a1aa8f7812954d36c8abb2a0b7948655e101
Nathan Atkins <atkinsnw@gmail.com>
2018-04-23 12:19:34 -0600

    Type hinging updates.

--- python/type_hinting/classes.py
+++ python/type_hinting/classes.py
@@ -1,10 +1,32 @@
-# simple classes used for type checking examples.
-class BaseClass(object):
-    def announce(self):
-        print('BaseClass')
+from abc import ABC, abstractmethod
+from typing import Union
 
 
-class ChildClass(BaseClass):
-    def announce(self):
-        print('ChildClass')
+class BaseClass(ABC):
+    def __init__(self, idx: int) -> None:
+        self.idx = idx  # type : int
 
+    @abstractmethod
+    def indexed_announce(self, msg: Union[str, None] = None) -> str:
+        pass
+
+
+class SubClassA(BaseClass):
+    def indexed_announce(self, msg: Union[str, None] = None) -> str:
+        return "Class A - {}: {}".format(msg, self.idx)
+
+
+class SubClassB(BaseClass):
+    def indexed_announce(self, msg: Union[str, None] = None) -> str:
+        return "Class B - {}: {}".format(msg, self.idx)
+
+
+def announce(cls: BaseClass, msg: str) -> str:
+    return cls.indexed_announce(msg)
+
+
+if __name__ == '__main__':
+    a = SubClassA(1)
+
+    print(a.indexed_announce("Hello"))
+    print(announce(a, "Hello"))
